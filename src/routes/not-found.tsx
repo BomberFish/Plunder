cb40eb8d3f5aefccb9a409bcf8fd60b83ec06d48
BomberFish <bomberfish@bomberfish.ca>
2024-12-04 20:32:23 -0500

    add a pirate gif to the 404 page

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -2,11 +2,31 @@ const NotFound: Component<{},{}> = function () {
   this.css = `
     text-align: center;
     height: 100vh;
+
+    img {
+      max-width: min(15em, 120px);
+      display: flex;
+      align-self: center;
+
+    }
+
+    * {
+      margin: 0;
+    }
+
+    h1 {
+      font-size: 4em;
+    }
+
+    p {
+      font-size: 1.25em;
+    }
   `;
 
   return (
     <div class="flex row center">
-      <div>
+      <div class="flex col gap-md h-center">
+        <img src="/assets/images/animated/computer2.gif" alt="Pirate typing at a computer" />
         <h1>404</h1>
         <p>Marooned on an island, matey?</p>
         <button class="primary" on:click={() => {
